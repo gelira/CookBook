237fc376789a2e182f57e4d7de71e8709e9b9380
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-12-17 11:10:07 -0300

    controller to add ingredientes

--- src/com/letscode/cookBook/controller/ListaIngredientes.java
+++ src/com/letscode/cookBook/controller/ListaIngredientes.java
@@ -1,4 +1,37 @@
 package com.letscode.cookBook.controller;
 
+import com.letscode.cookBook.domain.Ingrediente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaIngredientes {
+    private List<Ingrediente> ingredientes;
+
+    public ListaIngredientes() {
+        ingredientes = new ArrayList<>();
+    }
+
+    public void addIngrediente(Ingrediente ingrediente) {
+        ingredientes.add(ingrediente);
+    }
+
+    public boolean checkIngredienteExists(String nome) {
+        for (Ingrediente ingrediente : ingredientes) {
+            if (ingrediente.getNome().equalsIgnoreCase(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Ingrediente[] getIngredientes() {
+        Ingrediente[] ingredientesArray = new Ingrediente[ingredientes.size()];
+
+        for (int i = 0; i < ingredientes.size(); i ++) {
+            ingredientesArray[i] = ingredientes.get(i);
+        }
+
+        return ingredientesArray;
+    }
 }
