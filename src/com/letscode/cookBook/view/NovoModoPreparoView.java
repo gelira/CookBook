c8c43632faea0731532a173fd795dfe540a0e308
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-12-17 10:57:10 -0300

    asking for modoPreparo

--- src/com/letscode/cookBook/view/NovoModoPreparoView.java
+++ src/com/letscode/cookBook/view/NovoModoPreparoView.java
@@ -1,4 +1,40 @@
 package com.letscode.cookBook.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class NovoModoPreparoView {
+    private List<String> modoPreparo;
+
+    public NovoModoPreparoView() {
+        modoPreparo = new ArrayList<>();
+    }
+
+    public String[] askModoPreparo() {
+        int op;
+
+        do {
+            System.out.println("Nova instrução do modo de preparo:");
+
+            String instrucao = new Scanner(System.in).nextLine();
+            modoPreparo.add(instrucao);
+
+            System.out.print("Adicionar mais uma instrução? [0 - Não / qualquer coisa - Sim] ");
+            try {
+                op = new Scanner(System.in).nextInt();
+            }
+            catch (Exception e) {
+                op = 1;
+            }
+        } while (op != 0);
+
+        String[] modoPreparoArray = new String[modoPreparo.size()];
+
+        for (int i = 0; i < modoPreparo.size(); i ++) {
+            modoPreparoArray[i] = modoPreparo.get(i);
+        }
+
+        return modoPreparoArray;
+    }
 }
